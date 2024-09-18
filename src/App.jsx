83943f49d9f3a9d7acfb87b8f0217083a7d6e406
Hackeranuj <anujgahra244304@gmai.com>
2024-09-18 15:20:47 +0530

    Nike-Page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import HeroSection from "./components/Hero";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return(
+    <div>
+     <Navbar/>
+
+
+     
+     <HeroSection/>
+    </div>
+  )
+}
+
+export default App;
